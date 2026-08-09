5c5946d4fd864bcef32ac8ae5f1e9e500afa2aaa
hend <h53648176@gmail.com>
2026-08-09 21:57:03 +0900

    .

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 Hend | Front-End Developer. All Rights Reserved.</p>
+      <p>
+        <span className="computer">💻</span>
+        © 2026 Hend | Front-End Developer. All Rights Reserved.
+      </p>
     </footer>
   );
 }
